ea9696d3fa4e744b8670cb9bf9f1241948d3cff6
WINDOWS-LEGION-\Mario <mario.aguila_70@hotmail.com>
2020-09-16 09:45:07 -0400

    Dependencias

--- App/App.js
+++ App/App.js
@@ -27,8 +27,7 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name='Home' component={HomeScreen} />
         <Stack.Screen name="Tratamientos" component={TratamientoScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
