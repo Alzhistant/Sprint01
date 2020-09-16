ed6b64e9d0c9b0608fa99b9d1409b3feb6b5d178
PeaceM4k3r <dannittturra@hotmail.com>
2020-09-16 09:31:58 -0300

    Update Notifier.js

--- App/src/Notifier.js
+++ App/src/Notifier.js
@@ -1,6 +1,11 @@
 import * as Permissions from 'expo-permissions';
 import * as Notifications from  'expo-notifications';
-
+// Version 0.5 "estable", envio de notificaciones por medio de la app misma sin pruebas( a la espera del componente sentinel
+// para inicio de pruebas junto a la " alarma de fuga")
+// en android solo revibe notificaciones si la app "ExpoClient" con el proyecto montado esta en segundo plano
+// Confirma permisos de notificaciones y los pide de ser necesario
+// recive notificaciones mediante expo notifications tools
+// https://expo.io/notifications
 
 async function registerForPushNotificationsAsync(){
     const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
@@ -23,20 +28,18 @@ async function registerForPushNotificationsAsync(){
 export default function Notifier(){
    token = registerForPushNotificationsAsync();
 }
-
-
-{/*        THIS IS HOW WE HAVE TO "SEND" A NOTIFICATION IN CODE
-export function SendNotification(token,title,body,sound,priority){
+// importar siguiente libreria para hacer uso de notificaciones
+//import Notifier from './src/Notifier';
+export function SendNotification(token,titleNotification,bodyNotification,soundNotification,priorityNotification){
     const request = {
         to: token, // ExpoPushToken
         //data?: ,// JSON object 
-        title?: "NotificationsTitle", //string 
-        body?: "NotificationBody", //string
-        sound?: 'default', // null,
+        title?: titleNotification, //string
+        body?: bodyNotification, //string
+        sound?: soundNotification, //'default', // null,
         //ttl?: , //number, try again cada ttl? segundos
         //expiration?: ,//number, tiempo en segundos para expirar
-        priority?: 'default' // 'normal | 'high',
+        priority?: priorityNotification,// 'default' | 'normal | 'high',
         //badge?: , //number,
     }
 }
-*/}
