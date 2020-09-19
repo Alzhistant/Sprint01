e67aea3dfc0876e32e486831491cd024b767bb06
TheNogch <nicogoncha@gmail.com>
2020-09-19 18:12:43 -0300

    UI-Login implementados

--- App/src/screens/Treatment/Treatment.js
+++ App/src/screens/Treatment/Treatment.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-export default function Home() {
+export default function Treatment() {
     return (
         <View>
-            <Text>Home...</Text>
+            <Text>Tratamientos...</Text>
         </View>
     )
 }
