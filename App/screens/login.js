394fd084835ccf0d86c2ede18561a87e16adbe05
WINDOWS-LEGION-\Mario <mario.aguila_70@hotmail.com>
2020-09-19 02:02:48 -0400

    Firebase no contesta

--- App/screens/login.js
+++ App/screens/login.js
@@ -2,7 +2,7 @@ import React, {Component, useState, useEffect} from 'react';
 import { Image, View,Text,Button,TouchableHighlight,TouchableOpacity} from 'react-native';
 import { mainStyle, BotonRegister } from '../styles/styles';
 import auth from '@react-native-firebase/auth';
-import firebaseConfig from '../firebase/firebaseConfig'
+import {firebaseConfig} from '../firebase/firebaseConfig'
 import googleImg from '../assets/google.png'
 import { TextInput } from 'react-native-gesture-handler';
 import * as firebase from 'firebase'
@@ -67,13 +67,13 @@ export default class Login extends Component{
           <TouchableOpacity
             style={mainStyle.botonPrimario} 
             onPress={() => this.props.navigation.navigate('Register')}>
-            <Text style={mainStyle.textoBoton}>Registrarse</Text>
+            <Text style={mainStyle.textoBoton}>Registrarrse</Text>
           </TouchableOpacity>
           <Text>{"                                        "}</Text>
           <Text>{"                                        "}</Text>
           <Text style = {{color: 'black'}}>{"o inicie sesión con"}</Text>
           <Text>{"                                        "}</Text>
-          <TouchableOpacity onPress={() => this.signInWithGoogleAsync() }>
+          <TouchableOpacity onPress={() => /*this.signInWithGoogleAsync()*/console.log('Nope') }>
           <Image source={googleImg} style={mainStyle.botonCirculo} />
           </TouchableOpacity>
         </View>
--- App/screens/register.js
+++ App/screens/register.js
@@ -1,15 +1,14 @@
 import React, {Component, useState, useEffect} from 'react';
-import { Image, View,Text,Button,TouchableHighlight,TouchableOpacity} from 'react-native';
+import { Image, View,Text,TouchableHighlight,TouchableOpacity, ToastAndroid} from 'react-native';
 import { mainStyle, BotonRegister } from '../styles/styles';
 import auth from '@react-native-firebase/auth';
 import { firebaseConfig } from '../firebase/firebaseConfig'
 import { TextInput } from 'react-native-gesture-handler';
-import { useNavigation } from '@react-navigation/native'
-import * as firebase from 'firebase';
+//import * as firebase from 'firebase/app';
 
-if(!firebase.apps.length){
+/*if(!firebase.apps.length){
     firebase.initializeApp(firebaseConfig);
-}
+}*/
 
 
 export default class Register extends Component{
@@ -17,7 +16,8 @@ export default class Register extends Component{
         super();
         this.state = {nombre: '', apellido: '', correo: '', tel: '', pass1: '', pass2: ''};
     }
-    F1=()=>{
+    createUser=()=>{
+        var i;
         var Nombre = this.state.nombre;
         var Apellido = this.state.apellido;
         var Correo = this.state.correo;
@@ -25,20 +25,50 @@ export default class Register extends Component{
         var Pass1 = this.state.pass1;
         var Pass2 = this.state.pass2;
 
-        if(Pass1 == Pass2){
-            if(Pass1.length <= 8 )
-                alert("La contraseña es demasiado corta.")
-            /*else{
-
-            }*/
+        let Persona = [Nombre,Apellido,Correo,Telefono,Pass1,Pass2];
+        for(i=0;i<Persona.length;i++){
+            if(!Persona[i]){
+                ToastAndroid.showWithGravity(
+                    "Asegurese de rellenar todos los campos.",
+                    ToastAndroid.SHORT,
+                    ToastAndroid.BOTTOM
+                );
+                break;
+            }
         }
-            
-
-        else
-            alert("Las contraseñas no coinciden.")
+        if(Pass1 == Pass2){
+            if(Pass1.length < 8 ){
+                ToastAndroid.showWithGravity(
+                    "La contraseña es demasiado corta.",
+                    ToastAndroid.SHORT,
+                    ToastAndroid.BOTTOM
+            );
+            }
+            else{
+                auth().createUserWithEmailAndPassword(Persona[2], Persona[4]).then(() => {
+                    console.log('User account created & signed in!');
+                    this.props.navigate.navigation('Login');
+                }).catch(error => {
+                    if (error.code === 'auth/email-already-in-use') {
+                    console.log('That email address is already in use!');
+                    }
+                    if (error.code === 'auth/invalid-email') {
+                    console.log('That email address is invalid!');
+                    }
+                    console.error(error);
+                });
+            }
+        }
+        else{ 
+            ToastAndroid.showWithGravity(
+                "Las contraseñas no coinciden.",
+                ToastAndroid.SHORT,
+                ToastAndroid.BOTTOM
+            );
+        }
+        
     }
     render(){
-        //const {navigation} = useNavigation();
         return (
         <View style={mainStyle.container}>
                 <TextInput style={mainStyle.textInputRegister} placeholder="Nombre"
@@ -57,7 +87,7 @@ export default class Register extends Component{
                     onChangeText={tel => this.setState({tel})}
                     underlineColorAndroid={'transparent'}/>
 
-                <TextInput style={mainStyle.textInputRegister} placeholder="Contraseña"
+                <TextInput style={mainStyle.textInputRegister} placeholder="Contraseña (mín. 8 caracteres)"
                     onChangeText={pass1 => this.setState({pass1})}
                     secureTextEntry={true} underlineColorAndroid={'transparent'}/>
 
@@ -69,7 +99,7 @@ export default class Register extends Component{
                         <TouchableOpacity
                             style={mainStyle.botonPrimario} 
                             //onPress={() => navigation.navigate('Register')}
-                            onPress={this.F1}>
+                            onPress={this.createUser}>
                             <Text style={mainStyle.textoBoton}>Registrarse</Text>
                         </TouchableOpacity>
 
