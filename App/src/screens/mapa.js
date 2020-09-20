aa614e7432dcb5b75363e56a4bd514f5bc34f979
Lucciano99 <46768386+Lucciano99@users.noreply.github.com>
2020-09-19 19:36:41 -0500

    Update mapa.js

--- App/src/screens/mapa.js
+++ App/src/screens/mapa.js
@@ -7,10 +7,17 @@ export default function MapScreen({ navigation }) {
     return (
       <View style={mainStyle.container}>
         <MapView style={mainStyle.mapStyle}  > 
-        
+
+
+       <View style={mainStyle.container}>
+         <TouchableOpacity
+          style={mainStyle.botonPrimario}
+          onPress={() => navigation.navigation('Historial ubicacion')}>
+            <Text style={mainStyle.textoBoton}>Ver historial de ubicacion paciente</Text>
+          </TouchableOpacity>
+       </View>
+
         </MapView>
       </View>
     );
   }
-
-  
