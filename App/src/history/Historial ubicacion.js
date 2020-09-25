7435f7886bf3a30fde0807a146d8773914c4025f
Lucciano99 <46768386+Lucciano99@users.noreply.github.com>
2020-09-25 13:22:52 -0500

    Update Historial ubicacion.js

--- App/src/history/Historial ubicacion.js
+++ App/src/history/Historial ubicacion.js
@@ -3,26 +3,64 @@ import { View, Text, TouchableOpacity} from 'react native';
 import { mainStyle } from '../styles/styles';
 
 
-//Lo que se ve en pantallla
-export default function LocationSreen({ navigation }) {
-    return (
-    
-        <View>
-            
-            </View>
-    );
-}
+latP; //==>'34354'; ----Ejemplo de prueba 
+lonP; //==>'74245'; ----Ejemplo de pruebas  
+//time= new Date(); ----Seguir investigando
 
-class coordenada{
-    constructor(logitud,latitud,tiempo){
+  
+function capturarCoordenada(){
+  
+    function coordenada(longitud,latitud,tiempo){
         this.longitud=longitud;
         this.latitud=latitud;
         this.tiempo=tiempo;
     }
-    historia(){
-        //Posiblemente un for o un ciclo
-        return "El paciente estuvo: "+ this.tiempo, " minutos ","\n en las cordenadas: " + this.latitud, "\n" + this.longitud;
-    }
+
+var longitudCapturar=latP; 
+var latitudCapturar=lonP;
+//var tiempoCapturar=document.write(time.getHours());
+
+  
+coordenadaGuardada=new coordenada(longitudCapturar,latitudCapturar,tiempoCapturar);
+console.log(coordenadaGuardada);
+  
+insertar();
+
 }
 
-var Guardar_ubicacion=[];
+
+var array=[];
+
+function insertar(){
+  array.push(coordenadaGuardada); // Se estan guardando las coordenada como objeto
+  //console.log(array);
+}
+
+capturarCoordenada();
+
+
+
+
+/*
+class coordenada{
+    constructor(longitud,latitud,tiempo){
+        this.longitude=longitude;
+        this.latitude=latitude;
+        this.tiempo=tiempo;
+    }
+
+     historia(x,y){ 
+      
+      let Gurdar_ubicacion=[];
+      Guardar_ubicacion.push(x);
+      Guardar_ubicacion.push(y);
+    }
+
+
+}
+
+let pacienteCoordenada=new coordenada('Historial coordenada');
+pacienteCoordenada.historia('231','-321');
+
+*/
+
