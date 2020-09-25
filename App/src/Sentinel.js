939394f9ac805f6c38e92ea093d79be9e6ce40a6
ecarvallo <enriqueimmontemar@gmail.com>
2020-09-25 16:03:14 -0300

    Update Sentinel.js
    function remake

--- App/src/Sentinel.js
+++ App/src/Sentinel.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import {Platform, SyleSheet, Text, View} from 'react-native';
-import MapView from 'react-native-maps';
+//import MapView from 'react-native-maps';
+import * as BackgroundFetch from 'expo-background-fetch';
 
-function SentinelDDCalc(latP,lonP){
-	var radioM = 500;
-	
-	constructor(props){
+/*
+function SentinelDDCalc(latP,lonP,latR,lonR,radioM){
+	/*constructor(props){//objeto para la latitud y longitud de coordenadas
 		super(props);
 		
 		this.state = {
@@ -14,9 +14,9 @@ function SentinelDDCalc(latP,lonP){
 		}
 	}
 	
-	componentDidMount(){
+	componentDidMount(){//ubicación actual
 		this.watchId = navigator.geolocation.watchPosition{
-			(position) => {
+			(positio) => {
 				this.setState({
 				latitude: position.coords.latitude,
 				longitude: position.coords.longitude,
@@ -32,8 +32,8 @@ function SentinelDDCalc(latP,lonP){
 	var lat1 = latP;
 	var lon1 = lonP;
 
-	var lat2 = this.state.latitude;
-	var lon2 = this.state.longitude;
+	var lat2 = latR;
+	var lon2 = lonR;
 
 	var R = 6371e3; // radio de la tierra en metros
 	var fi1 = lat1 * Math.PI/180; // cambio de valores (fi, lambda) en radianes
@@ -50,6 +50,40 @@ function SentinelDDCalc(latP,lonP){
 	
 	//si la distancia entre las coordenadas es mayor o igual a radioM se manda la alerta
 	if(d>=radioM){
+		SendNotification(expoPushToken,"Paciente fuera de area"," ",'default','high');
+		return d;
+	}
+	else
+		return d;
+}*/
+
+function DistanciaCoordenadas(puntoSeguro, ubicacionPaciente){
+	var lat1 = ubicacionPaciente.latitude;
+	//console.log(lat1);
+	var lon1 = ubicacionPaciente.longitude;
+	//console.log(lon1);
+
+	var lat2 = puntoSeguro.latitude;
+	//console.log(lat2);
+	var lon2 = puntoSeguro.longitude;
+	//console.log(lon2);
+
+	var R = 6371e3; // radio de la tierra en metros
+	var fi1 = lat1 * Math.PI/180; // cambio de valores (fi, lambda) en radianes
+	var fi2 = lat2 * Math.PI/180;
+	var Dfi = (lat2-lat1) * Math.PI/180;
+	var Dlam = (lon2-lon1) * Math.PI/180;
+
+	var a = Math.sin(Dfi/2) * Math.sin(Dfi/2) +
+			  Math.cos(fi1) * Math.cos(fi2) *
+			  Math.sin(Dlam/2) * Math.sin(Dlam/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+	var d = R * c; // en metros
+	
+	//console.log(d);
+	
+	if(d>=puntoSeguro.distance){
 		SendNotification(expoPushToken,"Paciente fuera de area"," ",'default','high');
 		return d;
 	}
