bb499a816f6ebb9ef8359c137699843b5d44c775
Lucciano99 <46768386+Lucciano99@users.noreply.github.com>
2020-09-28 03:39:32 -0500

    Update LocationStack.js

--- App/src/navigation/LocationStack.js
+++ App/src/navigation/LocationStack.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
-import Location from '../screens/Location/Location'
+import Location from '../screens/Location/Location';
+import CurrentLocation from '../screens/Location/CurrentLocation';
+import SafeArea from '../screens/Location/SafeArea';
+import HistLocation from '../screens/Location/HistLocation';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +14,21 @@ export default function LocationStack() {
                 name="location"
                 component={Location}
                 options={{ title: "Ubicaciones" }}
+            />
+            <Stack.Screen 
+                name="currentLocation"
+                component={CurrentLocation}
+                options={{ title: "Ubicación Actual" }}
+            />
+            <Stack.Screen 
+                name="safeArea"
+                component={SafeArea}
+                options={{ title: "Determinar Área Segura" }}
+            />
+            <Stack.Screen 
+                name="histLocation"
+                component={HistLocation}
+                options={{ title: "Historial de Ubicaciones" }}
             />
         </Stack.Navigator>
     )
