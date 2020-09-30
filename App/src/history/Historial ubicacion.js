eb9d90b2d82e55388a2cd9d4f7c2b4e7c9e50a3e
Lucciano99 <46768386+Lucciano99@users.noreply.github.com>
2020-09-30 17:48:09 -0500

    Update Historial ubicacion.js

--- App/src/history/Historial ubicacion.js
+++ App/src/history/Historial ubicacion.js
@@ -1,7 +1,54 @@
 import * as React from 'react';
 import { View, Text, TouchableOpacity} from 'react native';
 import { mainStyle } from '../styles/styles';
+import firebase from 'firebase/app';
+import "firebase/storage";
+import "firebase/firestore";
 
+
+const db = firebase.firestore(firebaseApp);
+
+
+documentoReferente = db.collection("pacientes").doc("paciente-test");
+// Se hace referencia al archivo coleccion del documento paciente-test del cloud firestore
+
+
+export default function obtenerDatos(){
+
+    
+    documentoReferente.get().then(function(documento) {  
+    if (documento.exists) {
+        //ingresa a los datos del documentos
+        
+        console.log("Document data:", documento.data()); // Extrae los datos de "documento", el paramentro asignado a la funcion para ingresar a .doc()
+
+        documento.forEach(obj => {  //Realiza el recorrido del arreglo objeto de paciente-test, mostrando en pantalla cada atributo que contiene
+            Object.entries(obj).forEach(([createdAt,latitud,longitud ]) => {
+                console.log(`${createdAt} ${longitud} ${latitud}`);
+            });
+            console.log('-------------------');
+        });
+
+
+
+    } else {
+        // El caso que no se haya encontrado el documento
+        console.log("No such document!");
+    }
+    }).catch(function(error) {
+    console.log("Error getting document:", error);
+    });
+
+   
+}
+
+obtenerDatos();
+
+
+
+
+
+/*
 
 latP; //==>'34354'; ----Ejemplo de prueba 
 lonP; //==>'74245'; ----Ejemplo de pruebas  
@@ -41,29 +88,8 @@ function insertar(){
 
 capturarCoordenada();
 
-
-
-
-/*
-class coordenada{
-    constructor(longitud,latitud,tiempo){
-        this.longitude=longitude;
-        this.latitude=latitude;
-        this.tiempo=tiempo;
-    }
-
-     historia(x,y){ 
-      
-      let Gurdar_ubicacion=[];
-      Guardar_ubicacion.push(x);
-      Guardar_ubicacion.push(y);
-    }
-
-
-}
-
-let pacienteCoordenada=new coordenada('Historial coordenada');
-pacienteCoordenada.historia('231','-321');
-
 */
+
+
+
 
